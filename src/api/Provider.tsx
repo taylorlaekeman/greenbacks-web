952932e3a9a356fa3c799145c10bc8aa0d58d608
taylor <taylor.laekeman@gmail.com>
2021-04-07 22:10:09 -0400

    add loading indicator to authentication barrier and api provider

--- src/api/Provider.tsx
+++ src/api/Provider.tsx
@@ -14,6 +14,7 @@ import React, {
 } from 'react';
 
 import { useAuth } from 'auth';
+import LoadingIndicator from 'components/LoadingIndicator';
 
 const Provider: FunctionComponent<propTypes> = ({
   children,
@@ -31,7 +32,7 @@ const Provider: FunctionComponent<propTypes> = ({
     saveToken();
   }, [getAccessTokenSilently]);
 
-  if (!token) return <h1>loading</h1>;
+  if (!token) return <LoadingIndicator />;
 
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
