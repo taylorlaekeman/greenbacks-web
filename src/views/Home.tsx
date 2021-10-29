b9dcdfdd1cac34e2756031cda0d39f8268accf4e
taylor <taylor.laekeman@gmail.com>
2021-10-28 20:47:55 -0400

    eliminate transactions view

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,19 @@
 import React, { FunctionComponent } from 'react';
 
-import Transactions from 'views/Transactions';
+import useMonthlyTotal from 'hooks/useMonthlyTotal';
+import { Link } from 'routing';
+import datetime from 'utils/datetime';
+import TotalSpending from 'views/TotalSpending';
 
-const Home: FunctionComponent = () => <Transactions />;
+const Home: FunctionComponent = () => {
+  const now = datetime.now();
+  const total = useMonthlyTotal();
+  return (
+    <main>
+      <Link to="/connections">connections</Link>
+      <TotalSpending amount={total} month={`${now.year}-${now.month}`} />
+    </main>
+  );
+};
 
 export default Home;
