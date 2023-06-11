de8c6b7a04460f2b536eaace49c48872587207fb
taylor <taylor.laekeman@gmail.com>
2023-06-11 16:13:36 -0400

    add 3 and 6 month averages to average cash flow page

--- src/hooks/useAverageMonthlyEarning.ts
+++ src/hooks/useAverageMonthlyEarning.ts
@@ -2,12 +2,14 @@ import useAveragingPeriod from 'hooks/useAveragingPeriod';
 import useTransactionsByCategory from 'hooks/useTransactionsByCategory';
 import type { ApolloError } from 'hooks/useQuery';
 
-const useAverageMonthlyEarnings = (): {
+const useAverageMonthlyEarnings = ({ months }: { months?: number } = {}): {
   averageMonthlyEarning?: number;
   error?: ApolloError;
   isLoading: boolean;
 } => {
-  const { count, endIso: endDate, startIso: startDate } = useAveragingPeriod();
+  const { count, endIso: endDate, startIso: startDate } = useAveragingPeriod({
+    months,
+  });
   const { earning, error, isLoading } = useTransactionsByCategory({
     endDate,
     startDate,
